d5f7bf7304dd8ec209ca7c266cc6d000d9cf76fe
Eva Simpetru <eva.simpetru@gmail.com>
2022-09-23 18:02:37 +0300

    Integrated character movement in main

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -1,20 +1,55 @@
 package com.example.demo;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 
+
 public class MainActivity extends AppCompatActivity {
+
+//    float x;
+//    float y;
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event) {
+//        this.x = event.getX();
+//        this.y = event.getY();
+//
+//        View playerView = findViewById(R.id.player);
+//        View backView = findViewById(R.id.forest);
+//        System.out.println(backView.getX());
+//        float positionX = playerView.getX();
+//        float positionY = playerView.getY();
+//
+//        if (y >= 310 && y <= 710 && x >= 1900) {
+//            ObjectAnimator.ofFloat(playerView, "translationX", positionX, x - 100).setDuration(1000).start();
+//            ObjectAnimator.ofFloat(playerView, "translationY", positionY, y - 300).setDuration(1000).start();
+//            float backX = findViewById(R.id.forest).getX();
+////            System.out.println("before " + backX);
+//            ObjectAnimator.ofFloat(findViewById(R.id.forest), "translationX",  backX, -600).start();
+////            System.out.println("after " + findViewById(R.id.forest).getX());
+//        }
+//
+//        return true;
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         findViewById(R.id.AppName).setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.Q)
             @Override
             public void onClick(View view) {
+//                findViewById(R.id.splashScreen).setLeft(-100);
+
                 startActivity(new Intent(MainActivity.this, CharacterSelectActivity.class));
             }
         });
