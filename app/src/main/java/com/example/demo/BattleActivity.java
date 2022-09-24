7fbb585b4d4f26d3ad16e1045fdad313ec95730b
Eva Simpetru <eva.simpetru@gmail.com>
2022-09-24 11:12:53 +0300

    New stuff

--- app/src/main/java/com/example/demo/BattleActivity.java
+++ app/src/main/java/com/example/demo/BattleActivity.java
@@ -2,10 +2,15 @@ package com.example.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class BattleActivity extends AppCompatActivity {
 
@@ -23,159 +28,143 @@ public class BattleActivity extends AppCompatActivity {
     Button actionMos4;
     TextView vang_question;
     TextView mos_question;
-    TextView miron_question;
+    TextView miron_q;
+    TextView main_q;
+    Button fight_btn;
+    Button run_btn;
+    int random_run;
+    ImageView main_miron;
+
+    TextView miron_act1_used;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battle);
 
+        main_q = findViewById(R.id.main_question);
+        main_q.setVisibility(View.VISIBLE);
+        fight_btn = findViewById(R.id.fight);
+        run_btn = findViewById(R.id.run);
+
+        miron_q =findViewById(R.id.miron_question);
+        miron_q.setVisibility(View.GONE);
         actionMiron1 = findViewById(R.id.action1Miron);
         actionMiron2 = findViewById(R.id.action2Miron);
         actionMiron3 = findViewById(R.id.action3Miron);
         actionMiron4 = findViewById(R.id.action4Miron);
+        actionMiron1.setVisibility(View.GONE);
+        actionMiron2.setVisibility(View.GONE);
+        actionMiron3.setVisibility(View.GONE);
+        actionMiron4.setVisibility(View.GONE);
 
-//        actionMos1 = findViewById(R.id.action1Mos);
-//        actionMos2 = findViewById(R.id.action2Mos);
-//        actionMos3 = findViewById(R.id.action3Mos);
-//        actionMos4 = findViewById(R.id.action4Mos);
-//
-//        actionVang1 = findViewById(R.id.action1Vang);
-//        actionVang2 = findViewById(R.id.action2Vang);
-//        actionVang3 = findViewById(R.id.action3Vang);
-//        actionVang4 = findViewById(R.id.action4Vang);
-//
-        mos_question = findViewById(R.id.mos_question);
-        vang_question = findViewById(R.id.vang_question);
-        miron_question = findViewById(R.id.miron_question);
+        miron_act1_used = findViewById(R.id.miron_used1);
+        miron_act1_used.setVisibility(View.GONE);
 
-        if (CharacterSelectActivity.avatar == R.id.buton_vang) {
-            actionMos1.setVisibility(View.GONE);
-            actionMos2.setVisibility(View.GONE);
-            actionMos3.setVisibility(View.GONE);
-            actionMos4.setVisibility(View.GONE);
-            actionMiron1.setVisibility(View.GONE);
-            actionMiron2.setVisibility(View.GONE);
-            actionMiron3.setVisibility(View.GONE);
-            actionMiron4.setVisibility(View.GONE);
+        main_miron = findViewById(R.id.player_miron);
 
-            miron_question.setVisibility(View.GONE);
-//            mos_question.setVisibility(View.GONE);
-
-            actionVang1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    doAction(R.id.vang_question, 1);
-                }
-            });
-            actionVang2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    doAction(R.id.vang_question, 2);
-                }
-            });
-
-            actionVang3.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    doAction(R.id.vang_question, 3);
-                }
-            });
-
-            actionVang4.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    doAction(R.id.vang_question, 4);
-                }
-            });
-
+        if (CharacterSelectActivity.avatar == R.drawable.miron) {
+            main_miron.setVisibility(View.VISIBLE);
         }
-        if (CharacterSelectActivity.avatar == R.id.buton_miron) {
-            actionMos1.setVisibility(View.GONE);
-            actionMos2.setVisibility(View.GONE);
-            actionMos3.setVisibility(View.GONE);
-            actionMos4.setVisibility(View.GONE);
-            actionVang1.setVisibility(View.GONE);
-            actionVang2.setVisibility(View.GONE);
-            actionVang3.setVisibility(View.GONE);
-            actionVang4.setVisibility(View.GONE);
 
-            mos_question.setVisibility(View.GONE);
-            vang_question.setVisibility(View.GONE);
-
-            actionMiron1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    doAction(R.id.miron_question, 1);
+        run_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                random_run = new Random().nextInt(2);
+                if (random_run == 1) {
+                    goBackToGame();
                 }
-            });
+                else {
+                    fight_btn.setVisibility(View.GONE);
+                    run_btn.setVisibility(View.GONE);
+                    main_q.setVisibility(View.GONE);
 
-            actionMiron2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    doAction(R.id.miron_question, 2);
-                }
-            });
+                    if (CharacterSelectActivity.avatar == R.drawable.miron) {
 
-            actionMiron3.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    doAction(R.id.miron_question, 3);
-                }
-            });
+                        miron_q.setVisibility(View.VISIBLE);
+                        actionMiron1.setVisibility(View.VISIBLE);
+                        actionMiron2.setVisibility(View.VISIBLE);
+                        actionMiron3.setVisibility(View.VISIBLE);
+                        actionMiron4.setVisibility(View.VISIBLE);
+                    }
 
-            actionMiron4.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    doAction(R.id.miron_question, 4);
                 }
-            });
-        }
-        if (CharacterSelectActivity.avatar == R.id.buton_mos) {
-            actionVang1.setVisibility(View.GONE);
-            actionVang2.setVisibility(View.GONE);
-            actionVang3.setVisibility(View.GONE);
-            actionVang4.setVisibility(View.GONE);
-            actionMiron1.setVisibility(View.GONE);
-            actionMiron2.setVisibility(View.GONE);
-            actionMiron3.setVisibility(View.GONE);
-            actionMiron4.setVisibility(View.GONE);
+            }
+        });
 
-            vang_question.setVisibility(View.GONE);
-            miron_question.setVisibility(View.GONE);
+        fight_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (CharacterSelectActivity.avatar == R.drawable.miron) {
+                    fight_btn.setVisibility(View.GONE);
+                    run_btn.setVisibility(View.GONE);
+                    main_q.setVisibility(View.GONE);
 
-            actionMos1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    doAction(R.id.mos_question, 1);
+                    miron_q.setVisibility(View.VISIBLE);
+                    actionMiron1.setVisibility(View.VISIBLE);
+                    actionMiron2.setVisibility(View.VISIBLE);
+                    actionMiron3.setVisibility(View.VISIBLE);
+                    actionMiron4.setVisibility(View.VISIBLE);
                 }
-            });
-            actionMos2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    doAction(R.id.mos_question, 2);
-                }
-            });
 
-            actionMos3.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    doAction(R.id.mos_question, 3);
-                }
-            });
+            }
+        });
 
-            actionMos4.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    doAction(R.id.mos_question, 4);
-                }
-            });
-        }
+        actionMiron1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                actionMiron1.setVisibility(View.GONE);
+                actionMiron2.setVisibility(View.GONE);
+                actionMiron3.setVisibility(View.GONE);
+                actionMiron4.setVisibility(View.GONE);
+                miron_q.setVisibility(View.GONE);
+                miron_act1_used.setVisibility(View.VISIBLE);
+
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    public void run() {
+                    }
+                }, 1000);
+
+            }
+        });
+
+        actionMiron2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                actionMiron1.setVisibility(View.GONE);
+                actionMiron2.setVisibility(View.GONE);
+                actionMiron3.setVisibility(View.GONE);
+                actionMiron4.setVisibility(View.GONE);
+            }
+        });
+
+        actionMiron3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                actionMiron1.setVisibility(View.GONE);
+                actionMiron2.setVisibility(View.GONE);
+                actionMiron3.setVisibility(View.GONE);
+                actionMiron4.setVisibility(View.GONE);
+            }
+        });
+
+        actionMiron4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                actionMiron1.setVisibility(View.GONE);
+                actionMiron2.setVisibility(View.GONE);
+                actionMiron3.setVisibility(View.GONE);
+                actionMiron4.setVisibility(View.GONE);
+            }
+        });
 
     }
 
-    private void doAction(int avatar, int nr) {
-
+    private void goBackToGame() {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
     }
+
 
 }
